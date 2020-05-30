9fa47322e6717fb7c465b98ca0ca87e7b217a019
taku1010 <takumi.guitar1010@gmail.com>
2020-05-31 01:45:26 +0900

    Updates

---
+++ precache-manifest.1c7e661992c447b5ee963651a99f1f93.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fc97ca61431514ec96d8b8f9a143d6c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "999873f9fbcfdfa7bf99",
+    "url": "/portfolio/static/css/main.8265a1fb.chunk.css"
+  },
+  {
+    "revision": "311f5a70fe6c690d4ab5",
+    "url": "/portfolio/static/js/2.4b8dec16.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.4b8dec16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "999873f9fbcfdfa7bf99",
+    "url": "/portfolio/static/js/main.fdf3627f.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "959398ac9ac730c0199db2d9f41736ec",
+    "url": "/portfolio/static/media/backTexture.959398ac.jpg"
+  },
+  {
+    "revision": "28e7ee6755fe46d9bf7ebaa50410de32",
+    "url": "/portfolio/static/media/guitar1.28e7ee67.png"
+  },
+  {
+    "revision": "0c5c12f5a75821802b7eee9b8be5d093",
+    "url": "/portfolio/static/media/htmlImage.0c5c12f5.png"
+  },
+  {
+    "revision": "4c27c747a4d391ec9a0c7327c4fde25b",
+    "url": "/portfolio/static/media/keyvisual.4c27c747.jpg"
+  },
+  {
+    "revision": "0d8ed365842d251cd2bfa6e98d461d62",
+    "url": "/portfolio/static/media/nodeImage.0d8ed365.png"
+  },
+  {
+    "revision": "4ed2dd056c7417b74438b1cf23fb8987",
+    "url": "/portfolio/static/media/programming.4ed2dd05.jpg"
+  },
+  {
+    "revision": "03ea7bcd4ac098464428450608f4bce3",
+    "url": "/portfolio/static/media/pythonImage.03ea7bcd.png"
+  },
+  {
+    "revision": "c9e4c68ef0a33749e2e0eca6d1014cb8",
+    "url": "/portfolio/static/media/timetable.c9e4c68e.png"
+  },
+  {
+    "revision": "810cc624d981774791bd374a68ad0fb2",
+    "url": "/portfolio/static/media/white.810cc624.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.26b274166d59febb30c5aa91422273e0.js"
+  "/portfolio/precache-manifest.1c7e661992c447b5ee963651a99f1f93.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
